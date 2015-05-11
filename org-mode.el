dbc1ea97f0f8543d1ff86eeb0b5a41f47b5a3c1a
Sebastian Riedel <sebastian.riedel@dlr.de>
2015-05-11 15:13:46 +0200

    logdone, todo states and search files globalized

--- org-mode.el
+++ org-mode.el
@@ -3,13 +3,16 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 (setq org-agenda-files (list "~/org/gtd.org" "~/org/work.org" "~/org/private.org"))
+(setq org-agenda-text-search-extra-files (list "~/org/journal.org" "~/org/archive_gtd.org"))
 (setq org-default-notes-file (concat org-directory "/journal.org"))
 (setq org-archive-location "archive_gtd.org::datetree/")
 (setq org-tag-alist '(("OFFICE" . ?o) ("HOME" . ?h) ("READING" . ?r) ("TV" . ?t) ("COMPUTER" . ?c) ("LUNCHTIME" . ?l) ("FRIENDS" . ?f) ("PROJECT" . ?p)))
+(setq org-todo-keywords (list "TODO(t)" "STARTED(s)" "WAITING(w)" "APPT(a)" "|" "DONE(d)" "CANCELLED(c)" "DEFERRED(f)" "DELEGATED(g)"))
 (setq org-refile-targets (quote (("~/org/archive_gtd.org" :maxlevel . 2) ("~/org/someday.org" :maxlevel . 2) (org-agenda-files :maxlevel . 2))))
 (define-key global-map "\C-cc" 'org-capture)
 (setq org-startup-truncated nil)
 (setq org-startup-indented 1)
+(setq org-log-done 'time)
 (setq org-agenda-custom-commands (quote (("H" "Agenda and tagged TODOs" ((agenda "" nil) (tags-todo "{\\(OFFICE\\|HOME\\|COMPUTER\\|FRIENDS\\|READING\\|TV\\)}-SCHEDULED>=\"<2008-10-11>\"" nil)) nil nil) ("D" "Daily Action List" ((agenda "" ((org-agenda-ndays 1) (org-agenda-sorting-strategy (quote ((agenda time-up priority-down tag-up)))) (org-deadline-warning-days 0)))) nil))))
 
 ;;
